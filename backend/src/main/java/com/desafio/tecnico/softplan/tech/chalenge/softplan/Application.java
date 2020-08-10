6416b9f948b2f1db9941d74d1eb5a3c1a7dd51da
Jean <jeanlucafp@gmail.com>
2020-08-10 18:19:30 -0300

    bootstrap backend with sprint initializr

---
+++ backend/src/main/java/com/desafio/tecnico/softplan/tech/chalenge/softplan/Application.java
@@ -0,0 +1,13 @@
+package com.desafio.tecnico.softplan.tech.chalenge.softplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/desafio/tecnico/softplan/tech/chalenge/softplan/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.tecnico.softplan.tech.chalenge.softplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
